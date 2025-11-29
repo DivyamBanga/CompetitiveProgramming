dd9f0b06ec4cee2ab3e62f992c71bfb225804ac0
Div <78040076+DivyamBanga@users.noreply.github.com>
2025-11-29 02:58:33 -0500

    Time: 131 ms (5.10%) | Memory: 12.5 MB (10.45%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        total=0
+
+        totals=[]
+
+        while n!=1:
+            for i in str(n):
+                total+=int(i)**2
+            n=total
+            totals.append(n)
+            total=0
+
+            if len(totals)>500:
+                return False
+            
+        return True
